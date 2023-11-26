1cfaa42b3fcc454950009cc03a8280fd18e38197
SimonDarksideJ <darkside@zenithmoon.com>
2023-11-26 15:26:50 +0000

    Updated combobox example to demonstrate all events from the autocompletecombobox, including the new Selected event

--- ComboBox/ComboBoxChanged.cs
+++ ComboBox/ComboBoxChanged.cs
@@ -4,26 +4,32 @@
     {
         public void ComboBoxChangedEvent(string text)
         {
-
-            Debug.Log("ComboBox changed [" + text + "]");
+            Debug.Log($"ComboBox changed [{text}]");
         }
 
         public void AutoCompleteComboBoxChangedEvent(string text)
         {
-
-            Debug.Log("AutoCompleteComboBox changed [" + text + "]");
+            Debug.Log($"AutoCompleteComboBox changed [{text}]");
         }
 
         public void AutoCompleteComboBoxSelectionChangedEvent(string text, bool valid)
         {
+            Debug.Log($"AutoCompleteComboBox selection changed [{text}] and its validity was [{valid}]");
+        }
 
-            Debug.Log("AutoCompleteComboBox selection changed [" + text + "] and its validity was [" + valid + "]");
+        public void AutoCompleteComboBoxValidityChangedEvent(bool value)
+        {
+            Debug.Log($"AutoCompleteComboBox changed, is valid? [{value}]");
+        }
+
+        public void AutoCompleteComboBoxItemSelectedEvent(string text)
+        {
+            Debug.Log($"AutoCompleteComboBox selected item [{text}]");
         }
 
         public void DropDownChangedEvent(int newValue)
         {
-
-            Debug.Log("DropDown changed [" + newValue + "]");
+            Debug.Log($"DropDown changed [{newValue}]");
         }
     }
 }
